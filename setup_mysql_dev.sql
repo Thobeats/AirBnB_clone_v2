29b79c0294f48210216bcd2207a2de091dff6e2e
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-16 22:20:44 +0100

    set mysql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,16 +5,17 @@
 --- check if the user and database exists
 
 --- DATABASE hbnb_dev_db
-CREATE IF NOT EXISTS DATABASE hbnb_dev_db;
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 --- Use the DATABASE
 USE hbnb_dev_db;
 
 --- create the USER 
-CREATE IF NOT EXISTS USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 --- give the user privileges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 --- give the user privileges on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
