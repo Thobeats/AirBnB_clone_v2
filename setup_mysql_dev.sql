25eb08f3d1144983ec340700627cd2cb891fb777
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-16 22:28:12 +0100

    set up dev

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,24 +1,24 @@
---- CReate the database hbnb_dev_db
---- Create the user hbnb_dev with password hbnb_dev_pwd
---- hbnb_dev should have all the privileges on hbnb_dev_db
---- hbnb_dev should have SELECT privilege on performance_schema
---- check if the user and database exists
+-- Create the database hbnb_dev_db
+-- Create the user hbnb_dev with password hbnb_dev_pwd
+-- hbnb_dev should have all the privileges on hbnb_dev_db
+-- hbnb_dev should have SELECT privilege on performance_schema
+-- check if the user and database exists
 
---- DATABASE hbnb_dev_db
+-- DATABASE hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
---- Use the DATABASE
+-- Use the DATABASE
 USE hbnb_dev_db;
 
---- create the USER 
+-- create the USER 
 DROP USER IF EXISTS 'hbnb_dev'@'localhost';
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
---- give the user privileges
+-- give the user privileges
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
---- give the user privileges on performance_schema
+-- give the user privileges on performance_schema
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
 
---- Reload the grant table by flushing the privileges
+-- Reload the grant table by flushing the privileges
 FLUSH PRIVILEGES;
