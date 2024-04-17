1eb2a0d785402d8ed0b789a3aa763fe5743e53ff
Iyanu Dayisi <tobiy23@gmail.com>
2024-04-17 21:59:31 +0100

    new updates

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
@@ -14,3 +15,5 @@ class City(BaseModel, Base):
     state_id = Column(String(60),
                       ForeignKey('states.id'),
                       nullable=False)
+
+    places = relationship('City', cascade="all, delete", backref="city")
