362885c750840f3e93296714c1262b55869280b2
Iyanu Dayisi <tobiy23@gmail.com>
2024-05-04 01:01:53 +0100

    deploy web static

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+Write a Fabric script (based on the file 1-pack_web_static.py)
+that distributes an archive to your web servers,
+using the function do_deploy:
+"""
+from fabric.api import env, put, run
+from os.path import exists
+env.hosts = ['54.237.67.242', '100.25.0.50']
+
+
+def do_deploy(archive_path):
+    """
+    distributes an archive to your web servers
+    """
+    # check if the path exists
+    if not exists(archive_path):
+        return False
+
+    try:
+        # Upload the archive to the /tmp/ directory of the web server
+        put(archive_path, '/tmp/')
+
+        # get the archive file name with and without extension
+        archive_filename = archive_path.split('/')[-1]
+        archive_name = archive_filename.split('.')[0]
+
+        # Uncompress the archive to the folder
+        # /data/web_static/releases/<archive filename without extension>
+        run("mkdir -p /data/web_static/releases/{}".format(archive_name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}"
+            .format(archive_filename, archive_name))
+
+        # delete the archive from the server
+        run("rm /tmp/{}".format(archive_filename))
+
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("rm /data/web_static/current")
+
+        # create new symbolic link /data/web_static/current
+        run("ln -sf /data/web_static/releases/{} /data/web_static/current"
+            .format(archive_filename))
+        return True
+    except Exception:
+        return False
