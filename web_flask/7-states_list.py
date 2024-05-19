7f205657e6ac0909094d8b4b3c06ec509441c4ea
Iyanu Dayisi <tobiy23@gmail.com>
2024-05-19 21:52:03 +0100

    all web flask files

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -11,27 +11,29 @@ from models import storage
 from models.state import State
 from operator import attrgetter
 
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def all_states():
+    """
+    A function to get a list of all
+    the states
+    """
+    states = storage.all(State)
+    state_values = states.values()
+    states_final = sorted(state_values, key=attrgetter('name'))
+
+    return render_template('7-states_list.html', states=states_final)
+
+
+@app.teardown_appcontext
+def teardown_appcontext(self):
+    """
+    removes current SQLAlchemy Session after each request
+    """
+    storage.close()
+
 
 if __name__ == "__main__":
-    app = Flask(__name__)
-
-    @app.route("/states_list", strict_slashes=False)
-    def all_states():
-        """
-        A function to get a list of all
-        the states
-        """
-        states = storage.all(State)
-        state_values = states.values()
-        states_final = sorted(state_values, key=attrgetter('name'))
-
-        return render_template('7-states_list.html', states=states_final)
-
-    @app.teardown_appcontext
-    def close_storage(exception):
-        """
-        removes current SQLAlchemy Session after each request
-        """
-        storage.close()
-
     app.run(host="0.0.0.0", port=5000)
